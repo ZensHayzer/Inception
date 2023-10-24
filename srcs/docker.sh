77e887f51d31e981b4a8f7fb5c07f583950027f5
ajeanne <ajeanne@student.42.fr>
2023-10-24 20:06:02 +0200

    ok to test

--- srcs/docker.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt upgrade
-apt-get install  curl apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce
