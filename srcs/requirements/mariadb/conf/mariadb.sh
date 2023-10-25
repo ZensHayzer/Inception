121be2fb2bbc93ce4706da04aecff1d941a90f1d
ajeanne <ajeanne@student.42.fr>
2023-10-25 08:07:55 +0200

    mariadb

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,8 +1,12 @@
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS `${SQL_DATABASE}`;"
-mysql -e "CREATE USER IF NOT EXISTS `${SQL_USER}`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON `${SQL_DATABASE}`.* TO `${SQL_USER}`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+touch file
+chmod 777 file
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> file
+echo "FLUSH PRIVILEGES;" >> file
+echo "GRANT ALL ON *.* TO '$SQL_ROOT_USER'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> file
+echo "FLUSH PRIVILEGES;" >> file
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> file
+echo "GRANT ALL ON wordpress.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"  >> file
+echo "FLUSH PRIVILEGES;" >> file
+mysqld --user=mysql --verbose --bootstrap < file
+rm file
+exec mysqld
