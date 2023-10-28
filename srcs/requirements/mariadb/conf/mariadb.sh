405ec166011145f6d6a52900f7fa439c218bfa64
ajeanne <ajeanne@student.42.fr>
2023-10-28 14:02:37 +0200

    test

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -5,7 +5,7 @@ echo "FLUSH PRIVILEGES;" >> ext
 echo "GRANT ALL ON *.* TO '$SQL_ROOT_USER'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> ext
 echo "FLUSH PRIVILEGES;" >> ext
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> ext
-echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"  >> ext
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"  >> ext
 echo "FLUSH PRIVILEGES;" >> ext
 mysqld --user=mysql --verbose --bootstrap < ext
 rm ext
