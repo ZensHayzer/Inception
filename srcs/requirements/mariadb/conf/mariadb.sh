aba9006d86026f9bdd65049130e0a7e9725629fb
ajeanne <ajeanne@student.42.fr>
2023-10-28 12:04:28 +0200

    test

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,11 +1,11 @@
 touch ext
 chmod 777 ext
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> ext
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" >> ext
 echo "FLUSH PRIVILEGES;" >> ext
 echo "GRANT ALL ON *.* TO '$SQL_ROOT_USER'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> ext
 echo "FLUSH PRIVILEGES;" >> ext
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> ext
-echo "GRANT ALL ON wordpress.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"  >> ext
+echo "GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"  >> ext
 echo "FLUSH PRIVILEGES;" >> ext
 mysqld --user=mysql --verbose --bootstrap < ext
 rm ext
